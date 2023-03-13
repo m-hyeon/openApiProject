e8e1a20e90720730d6e53c0013a5922d94639b9e
솔로몬M-01-01-014 <M-01-01-014@DESKTOP-T8SRVO1>
2023-03-13 16:27:05 +0900

    2023-03-13 [okh] - 하드코딩 된 url,type을 properties로 전환

--- src/main/java/com/api/main/Main.java
+++ src/main/java/com/api/main/Main.java
@@ -1,6 +1,7 @@
 package com.api.main;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.PropertySource;
@@ -20,27 +21,42 @@ public class Main implements ApplicationRunner {
 	@Autowired
 	AirParser airParser;
 
+	
+	@Value("${com.api.acdnt.url}")
+	private String acdntUrl;
+	
+	@Value("${com.api.acdnt.type}")
+	private String acdntType;
 
+	@Value("${com.api.air.url}")
+	private String airUrl;
+	
+	@Value("${com.api.air.type}")
+	private String airType;
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		
+		scheduledRun();
+		
+	}
+	
+	
+	private void scheduledRun() throws Exception {
 		ApiUtil apiUtil = new ApiUtil();
 		
-		String url = "https://openapi.its.go.kr:9443/eventInfo?apiKey=c93967afe8df48fdbe73ce95b0b2a993&type=all&eventType=all&minX=126.764470&maxX=127.183797&minY=37.428387&maxY=37.701407&getType=json";
-		String data = apiUtil.apiGet(url, "json");
+		String url = acdntUrl;
+		String data = apiUtil.apiGet(url, acdntType);
 
 		System.out.println("data : " + data);
 
-		String url2 = "http://openAPI.seoul.go.kr:8088/6a4b587743736978373451775a5261/xml/TimeAverageAirQuality/1/5/20130615/";
-		String data2 = apiUtil.apiGet(url2, "xml");
+		String url2 = airUrl;
+		String data2 = apiUtil.apiGet(url2, airType);
 
 		System.out.println("data2 : " + data2);
 
 		
 		acdntParser.parser(data);
 		airParser.parser(data2);
-
-		
-		
 	}
-
 }
