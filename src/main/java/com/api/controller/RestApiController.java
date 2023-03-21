5d3fc5f4bb202944b5d2859a1b8108cf1b31051f
DESKTOP-I0T918U\user <sixthgkrsus@naver.com>
2023-03-22 01:02:07 +0900

    2023-03-22 okh - rest api - xml api success(org.jdom)

--- src/main/java/com/api/controller/RestApiController.java
+++ src/main/java/com/api/controller/RestApiController.java
@@ -3,38 +3,50 @@ package com.api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.service.ApiService;
 import com.api.vo.AcdntVO;
 import com.api.vo.AirVO;
 
+
 @RestController
 public class RestApiController {
 
 	@Autowired
 	ApiService apiService;
-	
-	
-	
+
 	@GetMapping("/acdnt")
-	public List<AcdntVO> acdntData(){
-		
+	public List<AcdntVO> acdntData() {
+
 		return apiService.selectAcdnt();
-		
+
 	}
+
 	
-	@GetMapping(path ="/air", produces = MediaType.APPLICATION_XML_VALUE)
-	public List<AirVO> airData(){
-		//https://velog.io/@garam0410/Java-OPEN-API-%ED%8C%8C%EC%8B%B1%ED%95%98%EA%B8%B0-XML
+	// XML 데이터 만들어 주기
+	@RequestMapping(value = "/air", method = RequestMethod.GET)
+	@ResponseBody
+	public AirVO sendxml() {
+
+		List<AirVO> air = apiService.selectAir();
+		AirVO airVO = new AirVO();
+		airVO.setAirVO(air);
 		
-		return ;
 		
+		return airVO;
 	}
-	
-	
-	
+
 }
